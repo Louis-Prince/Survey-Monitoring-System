713d6e07509e654530b2e52d5e228aec3f86ee8f
gikundiroHappy <gikundirohappy@gmail.com>
2026-01-06 17:52:28 +0200

    feat: input component and user creation

--- Frontend_field_monitoring/src/Components/Routes/index.jsx
+++ Frontend_field_monitoring/src/Components/Routes/index.jsx
@@ -1,10 +1,35 @@
 import { Routes, Route } from 'react-router-dom'
-import Home from '../../Pages/home'
+import CreateUser from '../../Pages/User_management/CreateUser'
+import CreatePassword from '../../Pages/Auth/createPassword'
+import Login from '../../Pages/Auth/login'
 
 export default function Index() {
   return (
     <Routes>
-        <Route path='/' Component={Home}/>
+        <Route path='/' Component={Login}/>
+        <Route path='/create-user' Component={CreateUser}/>
+        {/* <Route path='/create-password' Component={CreatePassword}/> */}
+        <Route
+        path="/create-password"
+        element={
+          <CreatePassword
+            mode="create"
+            title="Create New Password"
+            description="Please create a new password to continue"
+          />
+        }
+      />
+
+      <Route
+        path="/forgot-password"
+        element={
+          <CreatePassword
+            mode="reset"
+            title="Reset Password"
+            description="Enter a new password for your account"
+          />
+        }
+      />
     </Routes>
   )
 }
